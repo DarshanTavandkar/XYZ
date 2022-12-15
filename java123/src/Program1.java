2ba353a803e070280cf72a2d647f97f4d375233d
DarshanTavandkar <119282216+DarshanTavandkar@users.noreply.github.com>
2022-12-15 11:43:04 +0530

    change

---
+++ java123/src/Program1.java
@@ -0,0 +1,12 @@
+package javaprogram;
+
+public class Program1 {
+ //Print an Integer
+     public static void main(String args[]) {
+		
+    	 int a = 123456;
+    
+    	 System.out.println("Integer = "+ a);
+	}	
+	
+}
---
+++ java123/src/Program2.java
@@ -0,0 +1,14 @@
+package javaprogram;
+
+public class Program2 {
+ //Add Two Integer
+	public static void main(String args[])
+	{
+		int a=50;
+		int b=60;
+		
+		System.out.println("Abb to integer=" + (a+b));
+		
+	}
+	
+}
